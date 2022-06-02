b857fcc361ebbdbeeeef84cd4c28fa8e46975112
silvertejp89 <kriss.stockhaus@gmail.com>
2022-06-02 15:45:21 +0200

    add array of notes and mapping trough it to render notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
-import Header from "./Header"
-import Footer from "./Footer"
-import Note from "./Note"
+import Header from './Header';
+import Footer from './Footer';
+import Note from './Note';
+import notes from '../notes';
 
-function App () {
-    return (
-        <div>
-            <Header />
-            <Note/>
-            <Footer/>
-        </div>
-    )
+console.log(notes);
+
+function createEntry(note) {
+  return <Note key={note.key} title={note.title} content={note.content} />;
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map(createEntry)}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
+
+console.log('all is good');
